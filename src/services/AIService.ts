c4674bd7350426bb89c3681f5fbdb960ceb4c10e
Alex Medina <alexmedina@192.168.1.11>
2025-06-05 12:29:57 -0600

    ai

--- src/services/AIService.ts
+++ src/services/AIService.ts
@@ -4,7 +4,7 @@ import { openrouter } from '../lib/ai'
 export default {
   async generateRecipe(prompt: string){
     const result = streamText({
-      model: openrouter('nvidia/llama-3.3-nemotron-super-49b-v1:free'),
+      model: openrouter('meta-llama/llama-3.3-8b-instruct:free'),
       prompt,
       system: 'Eres un bartender con experienca',
       temperature: 0.5
